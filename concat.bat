1be8e6cf69fb704d1e57bd32618fe659583ab890
Ivan <i.mikhanchuk@gmail.com>
2016-10-03 13:06:11 +0300

    Add files via upload

--- concat.bat
+++ concat.bat
@@ -1,14 +1,26 @@
 @echo OFF
+setlocal EnableDelayedExpansion
 
-timeout /t 20
-cd C:\Users\confl\Desktop\T360\segments\%date:/=%\
+set d = "aa"
+timeout /t 40
 
 :loop
 	
-	call C:\Users\confl\Desktop\T360\list.bat
+	for /F "tokens=1*" %%a in ('fsutil fsinfo drives') do (
+	   for %%c in (%%b) do (
+	      for /F "tokens=3" %%d in ('fsutil fsinfo drivetype %%c') do (
+	         if %%d equ Removable (
+				cd C:\Users\confl\Desktop\T360
+				call list.bat
+				cd C:\Users\confl\Desktop\T360\segments\%date:/=%\
+				C:\Users\confl\Desktop\ffmpeg\bin\ffmpeg -y -f concat -i mylist.txt -c copy C:\Users\confl\Desktop\T360\streams\%date:/=%.mp4 -c copy %%c%date:/=%.mp4
+	         )
+	      )
+	   )
+	)
 
-	C:\Users\confl\Desktop\ffmpeg\bin\ffmpeg -y -f concat -i mylist.txt -c copy C:\Users\confl\Desktop\T360\Streams\%date:/=%.mp4 -c copy -ss E:\%date:/=%.mp4
 
-	timeout /t 20
+
+	timeout /t 40
 
 goto loop
--- stream.bat
+++ stream.bat
@@ -2,6 +2,6 @@ cd C:\Users\confl\Desktop\ffmpeg\bin\
 mkdir C:\Users\confl\Desktop\T360\segments\%date:/=%
 
 :loop
-ffmpeg -y -rtbufsize 500M -f dshow -i video="THETA UVC Blender":audio="@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{CF6D8BB5-5E46-4D77-BCE5-4D8B3B53054F}" -c:v libx264 -vf "scale=iw*.25:ih*.25, fps=15" -preset ultrafast -c:a aac -b:a 32k -f mpegts udp://7.206.92.181:1234 -c copy -c:v libx264 -vf "fps=15" -preset ultrafast -c:a aac -b:a 128k -f segment -segment_time 15 -strftime 1 -use_localtime 1 C:\Users\confl\Desktop\T360\segments\%date:/=%\%%Hh%%Mm%%Ss.mp4
+ffmpeg -y -rtbufsize 500M -f dshow -i video="THETA UVC Blender":audio="@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{CF6D8BB5-5E46-4D77-BCE5-4D8B3B53054F}" -c:v libx264 -vf "scale=iw*.25:ih*.25, fps=15" -preset ultrafast -c:a aac -b:a 32k -f mpegts udp://7.206.92.181:1234 -c copy -c:v libx264 -vf "fps=15" -preset ultrafast -c:a aac -b:a 128k -f segment -segment_time 30 -strftime 1 -use_localtime 1 C:\Users\confl\Desktop\T360\segments\%date:/=%\%%Hh%%Mm%%Ss.mp4
 timeout /t 10
 goto loop
